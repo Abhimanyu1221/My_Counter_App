c914bbfeb9facde190ea685ca56ca8bdf0e79b3d
Abhimanyu1221 <abhimanyuchaudhari99@gmail.com>
2026-01-31 12:06:40 +0530

    Added the counter logic, tests, and CI/CD workflow

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('checks if initial counter value is 0', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterElement = screen.getByTestId('count-value');
+  expect(counterElement.textContent).toBe("0");
 });
